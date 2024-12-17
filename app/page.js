0e8afa0149ac9de0da3ac8c93963e831bc4c0b15
Marcolussi <168032263+Marcolussi@users.noreply.github.com>
2024-12-17 23:59:30 +0100

    bug fix

--- app/page.js
+++ app/page.js
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <nmain>
+    <main>
       <button className="btn">Iniziamo</button>
-    </nmain>
+    </main>
   );
 }
